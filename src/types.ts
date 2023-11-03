8b1ea1aaa4f3c480850eab794af2e751e1b900e5
raultl12 <raultl6212@gmail.com>
2023-11-03 12:51:15 +0100

    Type que representa una notificacion. Closes #8

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,9 @@
+import { Tarea } from "./tarea";
 
 export type TipoTarea = "medica" | "deportiva" | "academica" | "laboral" | "reunion" | "otro";
 
 export type Notificacion = {
+    tareaRelacionada: Tarea,
     titulo : string,
     cuerpo : string
 }
