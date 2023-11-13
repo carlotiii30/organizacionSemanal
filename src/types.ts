956ec03dd436fce83840e261da2ef0b53c5b9b8b
raultl12 <raultl6212@gmail.com>
2023-11-13 11:06:05 +0100

    Creado objeto valor Actividad closes #13
    He decidido crear el tipo actividad para representar la informacion sobre las actividades que debe realizar el usuario. La fecha, duracion y si es aplazable o no, se ha incluido para facilitar la optimizacion cuando se aplique la logica de negocio

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type Actividad = {
+    titulo: string;
+    descripcion: string;
+    fechaRealizacion: Date;
+    duracionEstimada: number;
+    aplazable: boolean;
+    completada: boolean;
+  };
