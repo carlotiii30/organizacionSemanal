e4991612b47c58f11d2bcf5e027e8c2da2d0f0b4
raultl12 <raultl6212@gmail.com>
2023-11-14 15:39:52 +0100

    Eliminado el type Actividad (#13)
    Actividad no era un objeto valor, por lo que su mejor representación no es un type

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,3 @@
-export type Actividad = {
-    titulo: string;
-    descripcion: string;
-    fechaRealizacion: Date;
-    duracionEstimada: number;
-    aplazable: boolean;
-    completada: boolean;
-  };
-
 export type Tarea = {
   titulo: string;
   descripcion: string;
