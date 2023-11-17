c0c968ebced98dcd3d5c62b69520a87512547c16
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2023-11-17 09:17:03 +0100

    Configuracion de grunt para check de sintaxis

--- gruntfile.js
+++ gruntfile.js
@@ -1,23 +1,16 @@
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      uglify: {
-        options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-        },
-        build: {
-          src: 'src/<%= pkg.name %>.js',
-          dest: 'build/<%= pkg.name %>.min.js'
+        exec: {
+            typescript: 'tsc --noEmit lib/*.ts'
         }
-      }
     });
-
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.registerTask('check', 'exec:typescript');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
-
-  };
+    grunt.registerTask('default', function () {
+        // ...
+        console.log('This is the default task.');
+    });
+};
