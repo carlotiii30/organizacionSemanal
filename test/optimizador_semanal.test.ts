ca3b424d21537abb8a32b31adf34ca383a2cfceb
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2023-11-18 10:50:55 +0100

    Primer test: agregar una actividad. #24

---
+++ test/optimizador_semanal.test.ts
@@ -0,0 +1,17 @@
+// Tests
+
+import { OptimizadorSemanal } from '../src/optimizador_semanal';
+import { TipoActividad } from '../src/types';
+
+// Agregar una actividad
+describe('OptimizadorSemanal', () => {
+    it('debería agregar una actividad', () => {
+        const optimizador = new OptimizadorSemanal();
+        optimizador.agregarActividad({
+            TipoActividad: TipoActividad.ESTUDIO,
+            Tarea: ['Estudiar para el examen', new Date(2023, 9, 1, 10, 0)],
+        });
+
+        expect(optimizador.getActividades()).toHaveLength(1);
+    });
+}
