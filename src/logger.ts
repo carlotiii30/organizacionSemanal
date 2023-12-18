a7a64e32b3b7d890e0711c219726244f50e1005e
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2023-12-18 13:13:41 +0100

    Configuracion de logger. Closes #66
    Se ha configurado  para que registre los logs en un archivo

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,15 @@
 import pino from 'pino';
 
-const logger = pino();
+// Cargar variables de entorno desde el archivo .env
+require('dotenv').config();
+
+// Obtener configuraciones de Pino desde variables de entorno
+const logLevel = process.env.LOG_LEVEL || 'info';
+const logFilePath = process.env.LOG_FILE_PATH || 'logs.log';
+
+// Configurar Pino con las opciones obtenidas
+const logger = pino({
+  level: logLevel,
+}, pino.destination(logFilePath));
 
 export default logger;
