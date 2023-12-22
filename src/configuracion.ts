88b3f766d096dbd759c186ab4576d4e395f737a6
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2023-12-22 09:25:59 +0100

    Variables de entorno de GitHub, #75

--- src/configuracion.ts
+++ src/configuracion.ts
@@ -5,9 +5,18 @@ dotenv.config({ debug: true });
 export class Configuracion {
     get(key: string): string {
         const value = process.env[key];
+
         if (!value) {
+            // Si no se encuentra la variable en el archivo .env, intentar obtenerla de las variables de entorno de GitHub
+            const githubEnvVar = process.env[`GITHUB_ENV_${key.toUpperCase()}`];
+
+            if (githubEnvVar) {
+                return githubEnvVar;
+            }
+
             throw new Error(`La variable de entorno ${key} no está configurada.`);
         }
+
         return value;
     }
 }
