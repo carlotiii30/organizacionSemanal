146cd0985b9fe0236b863ea8f6756c619fadbf7e
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2023-12-25 12:01:15 +0100

    Añadidos verbos HTTP restantes. Listos para implementar. Closes #83

--- src/controlador.ts
+++ src/controlador.ts
@@ -8,4 +8,28 @@ export class Controlador {
     LoggerConfig.logger.debug('Comprobando estado del servidor');
     return 'El servidor está funcionando correctamente';
   }
+
+  @Post()
+  crearRecurso(): string {
+    // Lógica para crear un nuevo recurso
+
+    LoggerConfig.logger.info('Recurso creado');
+    return 'Recurso creado';
+  }
+
+  @Put(':id')
+  actualizarRecurso(@Param('id') id: string): string {
+    // Lógica para actualizar un recurso por su ID
+
+    LoggerConfig.logger.info(`Recurso con ID ${id} actualizado`);
+    return `Recurso con ID ${id} actualizado`;
+  }
+
+  @Delete(':id')
+  eliminarRecurso(@Param('id') id: string): string {
+    // Lógica para eliminar un recurso por su ID
+
+    LoggerConfig.logger.info(`Recurso con ID ${id} eliminado`);
+    return `Recurso con ID ${id} eliminado`;
+  }
 }
